7db9cb48b4def4c294b69bc626bb747dfdaaf63d
Ali Abdullah <abdull42335@gmail.com>
2025-04-17 14:24:37 +0500

    upload-files

---
+++ index.js
@@ -0,0 +1,20 @@
+// let paragraph = document.getElementById("para")
+
+// function changetext(){
+//     paragraph.replaceWith("This is a new Text")
+
+// }
+// addEventListener("mouseenter", changetext)
+
+
+// ***************calculator**************
+
+let buttons = document.querySelectorAll("button");
+let display = document.getElementById("inputdisplay");
+
+
+
+
+
+
+    
